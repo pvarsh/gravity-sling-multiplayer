839883b668940cc756760a5d4ec35b1dc49e3548
Peter Varshavsky <pvarsh@gmail.com>
2025-08-04 22:26:19 +0200

    Add player numbers to WS connections

--- src/backend/cmd/main.go
+++ src/backend/cmd/main.go
@@ -4,16 +4,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 
 	"github.com/gorilla/websocket"
 )
 
-var upgrader = websocket.Upgrader{
-	// TODO: restrict origins in production
-	CheckOrigin: func(r *http.Request) bool {
-		return true // Allow all origins for dev
-	},
-}
+var (
+	upgrader = websocket.Upgrader{
+		// TODO: restrict origins in production
+		CheckOrigin: func(r *http.Request) bool {
+			return true // Allow all origins for dev
+		},
+	}
+	playerNums   = make(map[*websocket.Conn]int)
+	playerNumsMu = &sync.Mutex{}
+)
 
 func main() {
 	http.HandleFunc("/", homeHandler)
@@ -24,6 +29,7 @@ func main() {
 		log.Fatalf("Could not start server: %s\n", err)
 	}
 }
+
 func wsHandler(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
@@ -32,20 +38,51 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer conn.Close()
 
-	log.Println("WebSocket connection established")
+	playerNum := assignPlayerNum(conn)
+	defer removePlayerNum(conn)
+
 	for {
 		messageType, msg, err := conn.ReadMessage()
 		if err != nil {
 			log.Printf("Error reading message: %s\n", err)
 			break
 		}
-		log.Printf("Received message: %s\n", msg)
+		log.Printf("Player %d: %s\n", playerNum, msg)
 
 		if err := conn.WriteMessage(messageType, msg); err != nil {
 			log.Printf("Error writing message: %s\n", err)
 			break
 		}
 	}
+}
+
+func assignPlayerNum(conn *websocket.Conn) int {
+	// Assign the lowest unused player number to the connection
+	playerNum := func() int {
+		playerNumsMu.Lock()
+		defer playerNumsMu.Unlock()
+		usedNums := make(map[int]bool)
+		for _, num := range playerNums {
+			usedNums[num] = true
+		}
+		playerNum := 1
+		for usedNums[playerNum] {
+			playerNum++
+		}
+		playerNums[conn] = playerNum
+		return playerNum
+	}()
+	log.Printf("Assigned player number %d to connection %s\n", playerNum, conn.RemoteAddr())
+	if err := conn.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("You are player %d", playerNum))); err != nil {
+		log.Printf("Error sending player number to connection %s: %s\n", conn.RemoteAddr(), err)
+	}
+	return playerNum
+}
+
+func removePlayerNum(conn *websocket.Conn) {
+	playerNumsMu.Lock()
+	defer playerNumsMu.Unlock()
+	delete(playerNums, conn)
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
