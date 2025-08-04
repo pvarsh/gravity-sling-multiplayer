d9648c5871f270ea9574d16ac2fe5a8e0d5b46ed
Peter Varshavsky <pvarsh@gmail.com>
2025-08-04 21:15:10 +0200

    Upgrade connection to websocket

--- src/backend/cmd/main.go
+++ src/backend/cmd/main.go
@@ -4,13 +4,45 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true // Allow all origins for dev
+	},
+}
 
 func main() {
 	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/ws", wsHandler)
 	log.Println("Starting server on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatalf("Could not start server: %s\n", err)
+	}
+}
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("Error upgrading connection: %s\n", err)
+		return
+	}
+	defer conn.Close()
+
+	log.Println("WebSocket connection established")
+	for {
+		messageType, msg, err := conn.ReadMessage()
+		if err != nil {
+			log.Printf("Error reading message: %s\n", err)
+			break
+		}
+		log.Printf("Received message: %s\n", msg)
+
+		if err := conn.WriteMessage(messageType, msg); err != nil {
+			log.Printf("Error writing message: %s\n", err)
+			break
+		}
 	}
 }
 
--- src/backend/go.mod
+++ src/backend/go.mod
@@ -1,3 +1,5 @@
 module github.com/pvarsh/gravity-sling-multiplayer
 
 go 1.24
+
+require github.com/gorilla/websocket v1.5.3
